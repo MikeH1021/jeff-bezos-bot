c93b5e4f13aad8e102a7d732cb5e586da21e37f7
Kyle Richter <kyle.richter0@gmail.com>
2022-02-01 01:56:14 -0600

    update bot status for 5 minutes

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import psutil
+import asyncio
 from discord import Color, Embed
 from discord.ext import commands
 
@@ -12,6 +13,18 @@ class Ping(commands.Cog):
     @commands.command()
     async def ping(self, ctx):
         """Ping bot"""
+        embed = await Ping.create_status_embed(self)
+        msg = await ctx.send(embed=embed)
+        await asyncio.sleep(1)
+        cnt = 265
+        while cnt > 0:
+            embed = await Ping.create_status_embed(self)
+            await msg.edit(embed=embed)
+            await asyncio.sleep(1)
+            cnt -= 1
+
+    async def create_status_embed(self):
+        """Helper function to create bot status embed"""
         uptime_seconds = round(
             (datetime.now() - self.start_time).total_seconds())
         uptime = await Ping.format_seconds(self, uptime_seconds)
@@ -19,14 +32,15 @@ class Ping(commands.Cog):
         cpu_usage = f'{psutil.cpu_percent()}%'
         mem_usage = f'{psutil.virtual_memory().percent}%'
         embed = Embed(
-            title='Jeff Bezos Bot Stats',
+            title='Jeff Bezos Bot Status',
+            description='This will update for 5 minutes',
             color=Color.orange()
         )
         embed.add_field(name='Ping', value=ping)
         embed.add_field(name='Uptime', value=uptime)
         embed.add_field(name='CPU Usage', value=cpu_usage, inline=False)
         embed.add_field(name='Memory Usage', value=mem_usage)
-        await ctx.send(embed=embed)
+        return embed
 
     async def format_seconds(self, time_seconds):
         """Formats some number of seconds into a string of format d days, x hours, y minutes, z seconds"""
