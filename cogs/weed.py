04e69fa09b116a6de11aa9f8896b861fa2391fbd
Michael Hernandez <mehernandez1021@comcast.net>
2022-02-01 18:26:12 -0500

    pm2 upgrades

--- cogs/weed.py
+++ cogs/weed.py
@@ -11,16 +11,19 @@ from datetime import datetime
 class weed(commands.Cog):
     def __init__(self, client):
         self.client = client
-        self.blink = Blink()
-        self.auth = Auth({"username": BLINK_USERNAME,
-                         "password": BLINK_PASSWORD}, no_prompt=True)
-        self.blink.auth = self.auth
-        self.blink.start()
+        if PY_ENV == 'PROD':
+            self.blink = Blink()
+            self.auth = Auth({"username": BLINK_USERNAME,
+                            "password": BLINK_PASSWORD}, no_prompt=True)
+            self.blink.auth = self.auth
+            self.blink.start()
 
     @commands.Cog.listener()
     async def on_ready(self):
-        pass
-        self.weed_loop.start()
+        if PY_ENV == 'PROD':
+            self.weed_loop.start()
+        else:
+            pass
 
     async def weed_helper(self, msg):
         camera = self.blink.cameras['Wiz']
@@ -29,33 +32,33 @@ class weed(commands.Cog):
         self.blink.refresh()
         current_time = datetime.now().timestamp()
         image_name = f'grow-log-{current_time}'
-        camera.image_to_file(f'./img/grow_log/{image_name}.jpg')
+        camera.image_to_file(
+            f'{os.getcwd()}/jeff-bezos-bot/img/grow_log/{image_name}.jpg')
         embed = Embed(
             title=msg,
             color=Color.green())
-        file = File(f'./img/grow_log/{image_name}.jpg', filename='image.jpg')
+        file = File(
+            f'{os.getcwd()}/jeff-bezos-bot/img/grow_log/{image_name}.jpg', filename='image.jpg')
         embed.set_image(url='attachment://image.jpg')
         return file, embed, image_name
 
     @tasks.loop(minutes=60)
     async def weed_loop(self):
-        # await ctx.send("Hourly grow tent updates, sponsored by Kyle Richter")
         msg = "Hourly Grow Tent Update... Sponsored by Daddy Bezos"
         file, embed, _ = await weed.weed_helper(self, msg)
         await self.client.get_channel(LOG_CH_ID).send(file=file, embed=embed)
 
     @commands.command()
     async def weed(self, ctx):
+        if ctx.channel.id == KYLE_LOG_CH_ID:
+            return
         temp = await ctx.send("Please wait..")
         msg = "Here's your pic, you filthy animal."
         file, embed, image_name = await weed.weed_helper(self, msg)
         await ctx.send(file=file, embed=embed)
-        os.remove(f'./img/grow_log/{image_name}.jpg')
+        os.remove(f'{os.getcwd()}/jeff-bezos-bot/img/grow_log/{image_name}.jpg')
         await temp.delete()
 
 
 def setup(client):
     client.add_cog(weed(client))
-
-# for name,camera in blink.cameras.items():
-#     print(name)
--- vars.py
+++ vars.py
@@ -10,27 +10,33 @@ CLIENT_TOKEN = os.getenv('CLIENT_TOKEN')
 BLINK_USERNAME = os.getenv('BLINK_USERNAME')
 BLINK_PASSWORD = os.getenv('BLINK_PASSWORD')
 
+# Environment
+PY_ENV = os.getenv('PY_ENV')
+
 # Tuya Authentication
 TUYA_API_KEY = os.getenv('TUYA_API_KEY')
 TUYA_API_SECRET = os.getenv('TUYA_API_SECRET')
 TUYA_DEVICE_ID = os.getenv('TUYA_DEVICE_ID')
 
+MY_ID = 450704927879069696
+KYLE_ID = 233262836506165252
 GUILD_ID = 933259754174824468
+KYLE_LOG_CH_ID = 933266710209908747
 LOG_CH_ID = 933266727519809568
 MOVIES_CH_ID = 937519245053530194
 
 # Plex Downloader
-#Radarr API Token
+# Radarr API Token
 R_TOKEN = os.getenv('R_TOKEN')
-#Radarr Host Url
+# Radarr Host Url
 R_HOST_URL = os.getenv('R_HOST_URL')
-#Sonarr API Token
+# Sonarr API Token
 S_TOKEN = None
-#Sonarr Host Url
+# Sonarr Host Url
 S_HOST_URL = None
-#TMDB API Token
+# TMDB API Token
 TMDB_TOKEN = os.getenv('TMDB_TOKEN')
-#Set your server name to be referenced in message responses
+# Set your server name to be referenced in message responses
 SERVER_NAME = 'Lil Nas-X'
-#Set your discord name to be referenced in message responses
+# Set your discord name to be referenced in message responses
 ADMIN_NAME = 'Mikeh1021#4070'
