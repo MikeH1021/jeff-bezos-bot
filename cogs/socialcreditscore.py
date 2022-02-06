c9ad7201a3ce68d83868d198551ca4c6f35e8447
Kyle Richter <kyle.richter0@gmail.com>
2022-02-06 13:37:34 -0600

    and the rest

--- cogs/socialcreditscore.py
+++ cogs/socialcreditscore.py
@@ -1,11 +1,15 @@
+from discord.commands import slash_command
 from discord.ext import commands
+from vars import GUILD_ID
 
 
 class Credit(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
+    @slash_command(name='ccp',
+                   description='Social Credit Score',
+                   guild_ids=[GUILD_ID])
     async def ccp(self, ctx):
         await ctx.send("(我们的) You have lost all social credit! Bad job citizen, bad ! ! ! You've been ban from the CCP, and shall be publically executed. Report to nearest police station for punishment! Failure to comply will result in immediate execution of nearest family member. Very bad! Glory to the CCP! 🇨🇳🇨🇳🇨🇳")
 
